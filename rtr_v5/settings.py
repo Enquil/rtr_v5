7d23ac0e07594c6cd17646a94811242bee308264
Enquil <jim.e.olesen@gmail.com>
2023-07-01 00:12:31 +0000

    copied staticfile handling settings from rtr_v3

--- rtr_v5/settings.py
+++ rtr_v5/settings.py
@@ -45,15 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
 
-    # cloudinary
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
-
     # allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # cloudinary
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 
     'home',
 ]
@@ -152,6 +152,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = (
+    'cloudinary_storage.storage.'
+    'StaticHashedCloudinaryStorage'
+)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
