1a0df6a5f94bf54e6243196cdc65ab4b3e731416
Enquil <jim.e.olesen@gmail.com>
2023-07-01 12:31:45 +0000

    added admin for UserProfile

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    '''
+        Only used this to check profiles were updating correctly
+        Should be removed before production
+    '''
+    list_display = ('id', 'user')
