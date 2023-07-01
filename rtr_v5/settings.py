3220a6bdee29b3f8dc65285db71f233a2122fe1f
Enquil <jim.e.olesen@gmail.com>
2023-07-01 00:57:54 +0000

    copied categories context processor from rtr_v3

--- rtr_v5/settings.py
+++ rtr_v5/settings.py
@@ -84,6 +84,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # categories context processor, see home app
+                'home.context_processors.categories',
             ],
         },
     },
