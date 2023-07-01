da346412d8dade77bb30d58d1cbf6acd8dbede63
Enquil <jim.e.olesen@gmail.com>
2023-07-01 22:36:21 +0000

    added allauth templates and 302 response test for when trying to access create_post while not logged in

--- create_post/test_views.py
+++ create_post/test_views.py
@@ -16,11 +16,25 @@ class CreatePostViewTest(TestCase):
         )
 
     def test_get_create_post_user_logged_in(self):
-
+        '''
+        Test access to create_post with logged in user
+        '''
+        # force login with user object
         user = User.objects.get(id=1)
+        self.client.force_login(user)
 
         response = self.client.get('/create_post/')
+        # Test if response.status_code is ok
         self.assertEqual(response.status_code, 200)
+        # Test if correct template is being used
         self.assertTemplateUsed(
             response, 'create_post/create_post.html', 'base.html'
         )
+
+    def test_get_create_post_user_anonymous(self):
+        '''
+        Test access to create_post when not logged in
+        '''
+        response = self.client.get('/create_post/')
+
+        self.assertEqual(response.status_code, 302)
