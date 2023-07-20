af3fe944ba761f6b94ec539b47d3574ba6a205c1
Enquil <jim.e.olesen@gmail.com>
2023-07-20 12:27:51 +0000

    put httpresponse inside if statement instead of outside

--- delete_actions/views.py
+++ delete_actions/views.py
@@ -8,12 +8,21 @@ from django.shortcuts import HttpResponseRedirect, reverse
 @login_required
 def delete_post(request, id):
 
-    template = '/profiles/profile.html'
-
     if request.method == 'POST':
+
+        # get post corresponding to id in request.POST and title
         post = Post.objects.get(id=id)
+        title = post.title
+
+        # if user is post-owner, delete it
         if post.author.id == request.user.id:
             post.delete()
+            # return message to confirm deletion
+            messages.add_message(
+                request, messages.SUCCESS,
+                f'Successfully deleted {post.title}'
+            )
+            return HttpResponseRedirect(reverse('profile'))
+        # if not
         else:
             raise PermissionDenied
-    return HttpResponseRedirect(reverse('profile'))
