62651253455ea88f215c6474aeea93ebd052a3e7
Enquil <jim.e.olesen@gmail.com>
2023-07-20 13:18:19 +0000

    created delete_comment view

--- delete_actions/views.py
+++ delete_actions/views.py
@@ -24,6 +24,32 @@ def delete_post(request, id):
                 request, messages.SUCCESS,
                 f'Successfully deleted {post.title}'
             )
+            # return to profile
+            return HttpResponseRedirect(reverse('profile'))
+        # if not
+        else:
+            raise PermissionDenied
+
+
+@login_required
+def delete_comment(request, id):
+
+    if request.method == 'POST':
+
+        # get comment corresponding to id in request.POST
+        comment = Comment.objects.get(id=id)
+
+        # if user is comment-owner, delete it
+        if comment.author.id == request.user.id:
+
+            comment.delete()
+
+            # return message to confirm deletion
+            messages.add_message(
+                request, messages.SUCCESS,
+                f'Successfully deleted comment'
+            )
+            # return to profile
             return HttpResponseRedirect(reverse('profile'))
         # if not
         else:
