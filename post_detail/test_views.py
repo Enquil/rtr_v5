9a696db368273e1c38e0af2c306a08d9920796c1
Enquil <jim.e.olesen@gmail.com>
2023-07-25 19:35:13 +0000

    added testing for handling comments with parents

--- post_detail/test_views.py
+++ post_detail/test_views.py
@@ -112,3 +112,70 @@ class TestPostView(TestCase):
         # assert status code is 302 and that anon_user is redirected to login
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, expected_url='/accounts/login/')
+
+    def test_comment_parent_as_author(self):
+
+        # Login user and get their post
+        user = User.objects.get(id=1)
+        post = Post.objects.get(id=1)
+        self.client.force_login(user)
+
+        # Create comment from new user
+        comment = Comment.objects.create(
+            author=User.objects.create(
+                username='test_user_two',
+                password='meow'
+            ),
+            post=post,
+            body='test comment',
+            approved=True,
+        )
+        comment.save()
+
+        # assert id and.is_top_level on comment for tests below
+        self.assertEqual(comment.id, 1)
+        self.assertTrue(comment.is_top_level)
+        # assert it has no children
+        self.assertEqual(len(comment.children), 0)
+
+        response = self.client.post(reverse('post_detail', args=[post.slug]), {
+            'author': user,
+            'body': 'test parent comment',
+            'parent': comment.id,
+        })
+
+        # should now = 1
+        self.assertEqual(len(comment.children), 1)
+        # should have the body of the comment created above
+        self.assertEqual(comment.children[0].body, 'test parent comment')
+        # Should not be a top level comment
+        self.assertFalse(comment.children[0].is_top_level)
+
+    def test_parent_comment_wrong_user(self):
+
+        user = User.objects.get(id=1)
+        post = Post.objects.get(id=1)
+
+        user_two = User.objects.create(
+            username='test_user_two',
+            password='meow'
+        )
+        self.client.force_login(user_two)
+
+        comment = Comment.objects.create(
+            author=user_two,
+            post=post,
+            body='test comment',
+            approved=True,
+        )
+        comment.save()
+
+        # try to post a comment with parent = comment, on post by user
+        response = self.client.post(reverse('post_detail', args=[post.slug]), {
+            'author': user_two,
+            'body': 'test parent comment',
+            'parent': comment.id,
+        })
+
+        # should return 403 since user is the post owner
+        self.assertEqual(response.status_code, 403)
